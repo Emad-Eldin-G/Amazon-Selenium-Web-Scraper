f80ccee9a777d857d391bdffeb77e50888a2b80b
Emad Eldin Gasser <omda.g@outlook.com>
2022-06-21 02:40:05 +0300

    🐛wrapped in try-excpet to reduce crashes

--- Scraper.py
+++ Scraper.py
@@ -2,19 +2,31 @@ from xml.dom.minidom import Element
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import *
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
-options = Options()
-options.set_preference('intl.accept_languages', 'en-GB')
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox()
 
-driver.get('https://www.amazon.sa/EVGA-220-GA-0750-X1-Modular-Warranty-Compact/dp/B07WW1XF51/')
+urls = ['https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF/','https://www.amazon.com/AMD-Ryzen-3600X-12-Thread-Processor/dp/B07SQBFN2D/', 'https://www.amazon.com/Intel-i7-12700K-Desktop-Processor-Unlocked/dp/B09FXNVDBJ/']
 
-price = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[7]/div[4]/div[4]/div[10]/div[1]/div[1]/span/span[2]/span[2]').text
-name  = driver.find_element(By.XPATH, '//*[@id="productTitle"]').text
+for url in urls:
+    driver.get(url)
+    name  = driver.find_element(By.XPATH, '//*[@id="productTitle"]').text   
 
-price = int(price.replace(',', ''))
+    try:
+        driver.get(url)
+        element = driver.find_element_by_class_name('twisterSwatchPrice a-size-base a-color-base')
+        price = element.get_attribute('innerHTML')
+    except NoSuchElementException:
+       try:
+          element = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[9]/div[6]/div[4]/div[10]/div[2]/div/table/tbody/tr[2]/td[2]/span[1]/span[1]')
+          price = element.get_attribute('innerHTML')
+       except NoSuchElementException:
+              print("Error finding element")
+              pass
 
-print(f'{name}/n{price}')
+    price = int(price.replace(',', ''))
+    print(f'\n{name}\nCosts:{price}\n')
+
 
 driver.quit()
