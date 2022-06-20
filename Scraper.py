2e91a7d199928e3e935227d50aa53c6d982d3987
Emad Eldin Gasser <omda.g@outlook.com>
2022-06-21 02:45:13 +0300

    🐛 wrapped in try-excpet to reduce crashes

--- Scraper.py
+++ Scraper.py
@@ -7,7 +7,7 @@ from time import sleep
 
 driver = webdriver.Firefox()
 
-urls = ['https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF/','https://www.amazon.com/AMD-Ryzen-3600X-12-Thread-Processor/dp/B07SQBFN2D/', 'https://www.amazon.com/Intel-i7-12700K-Desktop-Processor-Unlocked/dp/B09FXNVDBJ/']
+urls = ['https://www.amazon.com/AMD-Ryzen-5600X-12-Thread-Processor/dp/B08166SLDF/','https://www.amazon.com/AMD-Ryzen-3600X-12-Thread-Processor/dp/B07SQBFN2D/', 'https://www.amazon.com/dp/B09FWYK5M9/']
 
 for url in urls:
     driver.get(url)
@@ -17,16 +17,14 @@ for url in urls:
         driver.get(url)
         element = driver.find_element_by_class_name('twisterSwatchPrice a-size-base a-color-base')
         price = element.get_attribute('innerHTML')
+        print(f'\n{name}\nCosts:{price}\n')
     except NoSuchElementException:
        try:
           element = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[9]/div[6]/div[4]/div[10]/div[2]/div/table/tbody/tr[2]/td[2]/span[1]/span[1]')
           price = element.get_attribute('innerHTML')
+          print(f'\n{name}\nCosts:{price}\n')
        except NoSuchElementException:
               print("Error finding element")
               pass
-
-    price = int(price.replace(',', ''))
-    print(f'\n{name}\nCosts:{price}\n')
-
 
 driver.quit()
